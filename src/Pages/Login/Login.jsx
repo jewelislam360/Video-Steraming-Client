c1b41318f1afab8dc2c9a1b2135dd2fbe68ee28b
emondewan00 <dewanmohammademon@gmail.com>
2023-08-10 15:10:20 +0600

    input style add in utilitis

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -17,14 +17,14 @@ const Login = () => {
           <input
             type="email"
             id="email"
-            className="bg-gray-50/10  text-sm  focus:ring-blue-500  outline-none w-full block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 text-white"
+            className="cs-input"
             placeholder="Email"
             {...register("email", { required: true })}
           />
           <input
             type="password"
             id="password"
-            className="bg-gray-50/10  text-sm  focus:ring-blue-500  outline-none w-full block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 text-white"
+            className="cs-input"
             placeholder="Password"
             {...register("password", { required: true })}
           />
