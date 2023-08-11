da32b5db9295209550ea9ef406affb037828e890
emondewan00 <dewanmohammademon@gmail.com>
2023-08-11 16:22:58 +0600

    hallo

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,15 +1,7 @@
 import React, { useState } from "react";
 
 const Home = () => {
-  return (
-    <div className="h-screen flex justify-center items-center">
-      <div>
-        <h1 className="text-4xl">This is home Page </h1>
-        <button className="btn btn-primary">Increment</button>
-        <button className="btn btn-primary">Decrement</button>
-      </div>
-    </div>
-  );
+  return <div>hallo</div>;
 };
 
 export default Home;
