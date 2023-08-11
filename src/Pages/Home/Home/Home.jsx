07bac39d31d534c2c13ef8af64d7fe0e614d7f7d
abidhasanpranto52 <abidhasanpranto52@gmail.com>
2023-08-11 17:31:11 +0600

    Faq Section Added

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,14 +1,9 @@
 import React, { useState } from "react";
+import Faq from "./Faq/Faq";
 
 const Home = () => {
   return (
-    <div className="h-screen flex justify-center items-center">
-      <div>
-        <h1 className="text-4xl">This is home Page </h1>
-        <button className="btn btn-primary">Increment</button>
-        <button className="btn btn-primary">Decrement</button>
-      </div>
-    </div>
+    <Faq/>
   );
 };
 
