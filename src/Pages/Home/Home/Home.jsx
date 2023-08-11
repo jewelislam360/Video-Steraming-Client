d7b86766703b559214113daa32ca012a1b7afa18
jewelislam360 <jewelislam.it360@gmail.com>
2023-08-11 16:24:45 +0600

    bannner add

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,14 +1,9 @@
 import React, { useState } from "react";
+import Banner from "../Banner/Banner";
 
 const Home = () => {
   return (
-    <div className="h-screen flex justify-center items-center">
-      <div>
-        <h1 className="text-4xl">This is home Page </h1>
-        <button className="btn btn-primary">Increment</button>
-        <button className="btn btn-primary">Decrement</button>
-      </div>
-    </div>
+    <Banner></Banner>
   );
 };
 
--- src/components/Shared/Navbar/Navbar.jsx
+++ src/components/Shared/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import {FaStudiovinari} from 'react-icons/fa'
 const Navbar = () => {
     return (
         <>
-            <div className="shadow-md top-0 left-0 w-full fixed z-[100001]">
+            <div className="shadow-md bg-green-700 top-0 left-0 w-full fixed z-[100001]">
                 <div className="md:flex lg:flex py-4 justify-between max-w-screen-xl mx-auto">
                     <div className="ml-6 lg:ml-0 font-bold text-2xl cursor-pointer flex items-center font[Poppins]">
                         <div>
