1ed3113514268103815a990b1be30399d56777ae
jewelislam360 <jewelislam.it360@gmail.com>
2023-08-11 17:56:20 +0600

    navbar fix

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -2,13 +2,9 @@ import React, { useState } from "react";
 import Banner from "../Banner/Banner";
 
 const Home = () => {
-<<<<<<< HEAD
   return (
     <Banner></Banner>
   );
-=======
-  return <div>hallo</div>;
->>>>>>> 05a70b9b75dd5a5901f778d0dc0459d98f755570
 };
 
 export default Home;
--- src/components/Shared/Navbar/Navbar.jsx
+++ src/components/Shared/Navbar/Navbar.jsx
@@ -1,122 +1,37 @@
 import React from 'react';
-import {FaStudiovinari} from 'react-icons/fa'
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
-  
+    const navOption= <>
+    <li><a> Home</a></li>
+    <li><a>Movies</a></li>
+    <li><a>Tv series</a></li>
+    </>
+    
     return (
-<<<<<<< HEAD
-        <>
-            <div className="shadow-md bg-green-700 top-0 left-0 w-full fixed z-[100001]">
-                <div className="md:flex lg:flex py-4 justify-between max-w-screen-xl mx-auto">
-                    <div className="ml-6 lg:ml-0 font-bold text-2xl cursor-pointer flex items-center font[Poppins]">
-                        <div>
-                        <FaStudiovinari className='text-[#12f7ff] mr-1'/>
-                        </div>
-                        <span className="text-xl">pHiro</span>
-=======
         <div>
-            <div className="navbar">
-            <div className="navbar-start">
-              <div className="dropdown">
-                <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                </label>
-                <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow rounded-box w-40">
-                <li tabIndex={0}>
-                  <details> 
-                    <summary>Browse</summary>
-                    <ul className="p-2">
-                      <li><a>Submenu 1</a></li>
-                      <li><a>Submenu 2</a></li>
-                    </ul>
-                  </details>
-                </li>
-                <li tabIndex={0}>
-                  <details> 
-                    <summary>Home</summary>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow rounded-box w-40">
-                      <li><a>Submenu 1</a></li>
-                      <li><a>Submenu 2</a></li>
-                    </ul>
-                  </details>
-                </li>
-                <li tabIndex={0}>
-                  <details> 
-                    <summary>Movies</summary>
-                    <ul className="p-2">
-                      <li><a>Submenu 1</a></li>
-                      <li><a>Submenu 2</a></li>
-                    </ul>
-                  </details>
-                </li>
-                <li tabIndex={0}>
-                  <details> 
-                    <summary>TV Shows</summary>
-                    <ul className="p-2">
-                      <li><a>Submenu 1</a></li>
-                      <li><a>Submenu 2</a></li>
-                    </ul>
-                  </details>
-                </li>
-                <li><a>Blog</a></li>
-                </ul>
-              </div>
-              <div className='ml-6 lg:ml-0 font-bold text-2xl cursor-pointer flex items-center font[Poppins]'>
-                    <span className='text-3xl text-[#12f7ff] mr-1 pt-2'>
-                      <FaStudiovinari/>
-                      </span>
-                      Hero.
->>>>>>> 05a70b9b75dd5a5901f778d0dc0459d98f755570
-                    </div>
-                    <div className="hidden lg:flex">
-              <ul className="menu menu-horizontal px-1 ">
-              <li tabIndex={0}>
-                  <details> 
-                    <summary>Browse</summary>
-                    <ul className="p-2 w-40">
-                      <li><a>Submenu 1</a></li>
-                      <li><a>Submenu 2</a></li>
-                    </ul>
-                  </details>
-                </li>
-                <li tabIndex={0}>
-                  <details> 
-                    <summary>Home</summary>
-                    <ul className="p-2 w-40">
-                      <li><a>Submenu 1</a></li>
-                      <li><a>Submenu 2</a></li>
-                    </ul>
-                  </details>
-                </li>
-                <li tabIndex={0}>
-                  <details> 
-                    <summary>Movies</summary>
-                    <ul className="p-2 w-40">
-                      <li><a>Submenu 1</a></li>
-                      <li><a>Submenu 2</a></li>
-                    </ul>
-                  </details>
-                </li>
-                <li tabIndex={0}>
-                  <details> 
-                    <summary>TV Shows</summary>
-                    <ul className="p-2 w-40">
-                      <li><a>Submenu 1</a></li>
-                      <li><a>Submenu 2</a></li>
-                    </ul>
-                  </details>
-                </li>
-                <li><a>Blog</a></li>
-              </ul>
-            </div>
-            </div>
+            <div className="navbar fixed top-0 z-50 text-white    bg-slate-900/60 ">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <label tabIndex={0} className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </label>
+      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+        {navOption}
+      </ul>
+    </div>
+    <a className="btn btn-ghost text-red-700 font-bold text-xl">VIDBOX</a>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+        {navOption}
+    </ul>
+  </div>
+  <div className="navbar-end">
+    <Link className="px-4 bg-red-600 py-[10px] rounded-[1px] text-white">Login</Link>
+  </div>
+</div>
             
-            <div className="navbar-end">
-            <div className="form-control">
-              <input type="text" placeholder="Search" className="input input-bordered border-2 border-base-100 h-8 mr-2 md:w-auto w-36 lg:w-auto xl:w-auto" />
-            </div>
-              <a className="btn btn-sm btn-outline border-base-100 text-white border-2">Login</a>
-            </div>
-          </div>
         </div>
     );
 };
