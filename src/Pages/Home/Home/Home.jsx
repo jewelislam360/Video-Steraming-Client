0045ab5403525402b0d29026d46e8f53726d1876
jewelislam360 <jewelislam.it360@gmail.com>
2023-08-14 01:31:32 +0600

    card add

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import Faq from "../Faq/Faq";
 import Plan from "../Plan/Plan";
 import Contact from "../../Contact/Contact";
+import AllMovies from "../AllMovies/AllMovies";
 
 const Home = () => {
   return (
     <div>
+    <AllMovies></AllMovies>
     <Faq></Faq>
     <Plan></Plan>
     <Contact/>
