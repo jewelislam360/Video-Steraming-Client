27d1d75d5b6914c636ab23eb8a67cb3a954281bf
sawonhossain34 <sawonhossain3491@gmail.com>
2023-08-14 01:06:56 +0600

    footer

--- src/components/Shared/Footer/Footer.jsx
+++ src/components/Shared/Footer/Footer.jsx
@@ -1,79 +1,129 @@
-
-import { Link } from "react-router-dom";
-import { FaFacebook, FaTwitter, FaWhatsapp, FaInstagram } from 'react-icons/fa';
-import movies from "../Footer/imgs/movies-1.jpg";
-import movies2 from "../Footer/imgs/movies-2.jpg"
-import movies3 from "../Footer/imgs/movies-2.jpg"
-
+import { FaAngleRight, FaFacebook, FaSkype, FaTwitter, FaInstagram } from 'react-icons/fa';
 
 const Footer = () => {
     return (
-        // footer
-        <footer className="bg-gray-900 text-white ">
-            <div className="">
-                <div className="grid grid-cols-12 gap-8 mx-5 py-10">
-                    {/* left side */}
-                    <div className="col-span-12 md:col-span-4">
-                        <a href="/" className="text-6xl font-bold pb-8">Netfliexy</a>
-                        <p className="pt-10">Eiusmod tempor incididunt ut la abore et minim ven exerc itation ulla mco lboris naliquip ex ea comm.</p>
-                        <div className='flex mt-8 text-4xl'>
+        <footer>
+            {/* /Top section */}
+            <div className="py-10  bg-[#272829] text-white">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 p-10">
+                    <div className='mr-3 text-white'>
+                        <h2 className='text-3xl font-bold pb-10'>VidBox</h2>
+                        <p className='text-xl pb-10'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde, reiciendis.</p>
+                        <div className='flex  '>
+                            <a className=' flex items-center justify-center  mr-7 text-sky-600 hover:text-white' href=""><FaFacebook className='text-2xl' /></a>
+                            <a className=' flex items-center justify-center mr-7 text-sky-600  hover:text-white' href=""><FaSkype className='text-2xl' /></a>
+                            <a className='flex items-center justify-center mr-7 text-blue-700  hover:text-white' href=""><FaTwitter className='text-2xl' /></a>
+                            <a className='flex items-center justify-center mr-7 text-red-500  hover:text-white' href=""><FaInstagram className='text-2xl' /></a>
+                        </div>
 
-                            <Link to="/#"><FaTwitter className=' mr-4 hover:text-white  text-sky-600' /></Link>
-                            <Link to="/#"><FaFacebook className=' mr-4 hover:text-white  text-blue-600' /></Link>
-                            <Link to="/#"><FaWhatsapp className=' mr-4 hover:text-white  text-lime-600' /></Link>
-                            <Link to="/#"><FaInstagram className=' mr-4 hover:text-white  text-red-600' /></Link>
-                        </div>
                     </div>
-                    {/* right side */}
-                    <div className=" md:col-span-8 mx-4">
-                        <div className="col-span-12 text-2xl font-bold flex flex-wrap mb-8  ">
-                            <a href="#" className=" hover:text-red-600 mr-6">Action</a>
-                            <a href="#" className=" hover:text-red-600  mr-6">Comedy</a>
-                            <a href="#" className=" hover:text-red-600  mr-6">Drama</a>
-                            <a href="#" className=" hover:text-red-600  mr-6">Kids Shows</a>
-                            <a href="#" className=" hover:text-red-600  mr-6">Music & concerts</a>
-                            <a href="#" className=" hover:text-red-600  mr-6">Family Movies</a>
-                            <a href="#" className=" hover:text-red-600  mr-6">Horror</a>
-                            <a href="#" className=" hover:text-red-600  mr-6">Classics</a>
-                        </div>
-                        <div className="col-span-12 ">
-                            <div className="flex  justify-between gap-5 mb-8 space-x-16 ">
-                                <img className="w-[200px] h-[100px] border-2 border-red-600 transform transition duration-500 hover:scale-110 " src={movies} alt="Movies" />
-                                <img className="w-[200px] h-[100px] border-2 border-red-600 transform transition duration-500 hover:scale-110 " src={movies2} alt="Movies" />
-                                <img className="w-[200px] h-[100px] border-2 border-red-600 transform transition duration-500 hover:scale-110 " src={movies3} alt="Movies" />
-                                <img className="w-[200px] h-[100px] border-2 border-red-600 transform transition duration-500 hover:scale-110 " src={movies3} alt="Movies" />
+                    {/* Explore section */}
+                    <div className='mr-3'>
+
+                        <h2 className='text-3xl font-bold pb-6'>Explore</h2>
+                        <div className='flex justify-between gap-2'>
+                            <div className=''>
+                                <ul className="">
+                                    <li className="pb-6 flex  font-bold items-center"><FaAngleRight className='text-red-600' /><a className='hover:text-red-600' href="">Home</a></li>
+                                    <li className="pb-6 flex  font-bold items-center"><FaAngleRight className='text-red-600' /><a className='hover:text-red-600' href="">Terms Of Uses </a></li>
+                                    <li className="pb-6 flex  font-bold items-center"><FaAngleRight className='text-red-600' /><a className='hover:text-red-600' href="">Contact Us</a></li>
+                                    <li className="pb-6 flex  font-bold items-center"><FaAngleRight className='text-red-600' /><a className='hover:text-red-600' href="">Our Team</a></li>
+                                </ul>
+                            </div>
+                            <div>
+                                <ul className="">
+                                    <li className="pb-6 flex  font-bold items-center"><FaAngleRight className='text-red-600' /><a className='hover:text-red-600' href="">Privacy Policy</a></li>
+                                    <li className="pb-6 flex  font-bold items-center"><FaAngleRight className='text-red-600' /><a className='hover:text-red-600' href="">Help Center</a></li>
+                                    <li className="pb-6 flex  font-bold items-center"><FaAngleRight className='text-red-600' /><a className='hover:text-red-600' href="">Subscribe</a></li>
+                                    <li className="pb-6 flex  font-bold items-center"><FaAngleRight className='text-red-600' /><a className='hover:text-red-600' href="">Faq</a></li>
+                                </ul>
                             </div>
                         </div>
-                        <div className="col-span-12 text-xl  mb-8 ">
-                            <a href="#" className=" hover:text-red-600 pr-5  border-r-2">Report a Bug</a>
-                            <a href="#" className=" hover:text-red-600 px-5 border-r-2">Request a Feature</a>
-                            <a href="#" className=" hover:text-red-600  px-5 border-r-2">Content Grievance</a>
-                            <a href="#" className=" hover:text-red-600  px-5 border-r-2">Movie Request</a>
-                            <a href="#" className=" hover:text-red-600  px-5 border-r-2">Submit Your Story</a>
-                            <a href="#" className=" hover:text-red-600  px-5 border-r-2">Privacy Policy</a>
-                            <a href="#" className=" hover:text-red-600  px-5 border-r-2">Terms of Services</a>
-                            <a href="#" className=" hover:text-red-600  px-5 ">Support</a>
+                    </div>
+                    {/* Company section */}
+                    <div className='mr-3'>
+                        <h2 className='text-3xl font-bold pb-6'>Company</h2>
+                        <div className='flex justify-between gap-2'>
+                            <div className=''>
+                                <ul className="">
+                                    <li className="pb-6 flex  font-bold items-center"><FaAngleRight className='text-red-600' /><a className='hover:text-red-600' href="">Company</a></li>
+                                    <li className="pb-6 flex  font-bold items-center"><FaAngleRight className='text-red-600' /><a className='hover:text-red-600' href="">Terms Of Uses </a></li>
+                                    <li className="pb-6 flex  font-bold items-center"><FaAngleRight className='text-red-600' /><a className='hover:text-red-600' href="">Contact Us</a></li>
+                                    <li className="pb-6 flex  font-bold items-center"><FaAngleRight className='text-red-600' /><a className='hover:text-red-600' href="">Our Team</a></li>
+                                </ul>
+                            </div>
+                            <div>
+                                <ul className="">
+                                    <li className="pb-6 flex  font-bold items-center"><FaAngleRight className='text-red-600' /><a className='hover:text-red-600' href="">Privacy Policy</a></li>
+                                    <li className="pb-6 flex  font-bold items-center"><FaAngleRight className='text-red-600' /><a className='hover:text-red-600' href="">Help Center</a></li>
+                                    <li className="pb-6 flex  font-bold items-center"><FaAngleRight className='text-red-600' /><a className='hover:text-red-600' href="">Subscribe</a></li>
+                                    <li className="pb-6 flex  font-bold items-center"><FaAngleRight className='text-red-600' /><a className='hover:text-red-600' href="">Faq</a></li>
+                                </ul>
+                            </div>
                         </div>
+                    </div>
+                    {/* Download app section */}
+                    <div>
+                        <h2 className='text-3xl font-bold pb-6'>Downloads App</h2>
+                        <p className='text-xl pb-10'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde, reiciendis.</p>
+                        <div className='flex gap-2 '>
+                            <a href="" target="_blank"
+                                className="flex items-center justify-center w-full px-5 py-3 text-center text-white bg-black rounded-2xl sm:rounded-2xl md:w-auto"
+                                rel="noreferrer">
+                                <svg className="w-7" viewBox="-9 0 274 274" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <g>
+                                        <path
+                                            d="M188.81319,178.874645 C221.272218,161.051727 245.880297,147.470853 248.001319,146.415618 C254.78648,142.806714 261.79324,133.256838 248.001319,125.838536 C243.548228,123.506467 219.573289,110.347687 188.81319,93.3795092 L146.171146,136.443648 L188.81319,178.874645 Z"
+                                            fill="#FFD900"></path>
+                                        <path
+                                            d="M146.171146,136.443648 L10.3940643,273.286517 C13.5808739,273.708611 17.1792251,272.864423 21.4212696,270.532353 C30.3274526,265.657168 124.739324,214.098388 188.81319,178.885198 L146.171146,136.443648 Z"
+                                            fill="#F43249"></path>
+                                        <path
+                                            d="M146.171146,136.443648 L188.81319,93.5905562 C188.81319,93.5905562 30.9711459,7.45172685 21.4212696,2.36549437 C17.8229184,0.233919759 13.7919209,-0.399221214 10.1830173,0.233919759 L146.171146,136.443648 Z"
+                                            fill="#00EE76"></path>
+                                        <path
+                                            d="M146.171146,136.443648 L10.1830173,0.233919759 C4.6641385,1.51075405 0,6.38593954 0,16.3579099 C0,32.270853 0,244.003747 0,257.162527 C0,266.290309 3.60890354,272.864423 10.3940643,273.497564 L146.171146,136.443648 Z"
+                                            fill="#00D3FF"></path>
+                                    </g>
+                                </svg>
+                                <div className="flex flex-col ml-2 leading-4 text-left md:ml-3">
+                                    <span className="text-sm text-white">Get it on</span>
+                                    <span className="text-base font-semibold text-white">
+                                        Play Store
+                                    </span>
+                                </div>
+                            </a>
+                            <a href=""
+                                target="_blank"
+                                className="flex items-center justify-center w-full px-5 py-3 text-center text-white bg-black rounded-2xl sm:rounded-2xl md:w-auto"
+                                rel="noreferrer">
+                                <svg className="w-7" viewBox="0 0 40 45" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <g>
+                                        <path
+                                            d="M32.6226 23.7016C32.6026 20.0267 34.2591 17.253 37.6118 15.2103C35.7359 12.5167 32.902 11.0347 29.1601 10.7443C25.6177 10.464 21.7461 12.8171 20.3292 12.8171C18.8324 12.8171 15.3998 10.8445 12.7057 10.8445C7.13769 10.9346 1.22048 15.3004 1.22048 24.1822C1.22048 26.8057 1.69945 29.516 2.65738 32.3131C3.93461 35.988 8.54465 45 13.3542 44.8498C15.8688 44.7897 17.645 43.0574 20.9179 43.0574C24.091 43.0574 25.7375 44.8498 28.5414 44.8498C33.3909 44.7797 37.5619 36.5888 38.7793 32.9039C32.2733 29.8298 32.6226 23.8919 32.6226 23.7016ZM26.9748 7.25968C29.6989 4.01535 29.4494 1.06142 29.3696 0C26.9648 0.140187 24.1808 1.64219 22.5943 3.49466C20.848 5.4773 19.8203 7.93058 20.0398 10.6943C22.6442 10.8945 25.019 9.55274 26.9748 7.25968Z"
+                                            fill="white"></path>
+                                    </g>
+                                    <defs>
+                                        <clipPath id="clip0">
+                                            <rect width="40" height="45" fill="white"></rect>
+                                        </clipPath>
+                                    </defs>
+                                </svg>
+                                <div className="flex flex-col ml-2 leading-4 text-left md:ml-3">
+                                    <span className="text-sm text-white">Get it on</span>
+                                    <span className="text-base font-semibold text-white">
+                                        App Store
+                                    </span>
+                                </div>
+                            </a>
+                        </div>
+
                     </div>
                 </div>
             </div>
-
-            {/* bottom */}
-            <hr className="text-base-300 shadow-sm" />
-            <div class="text-center flex flex-col justify-between items-center mx-5 py-10 md:flex-row md:items-center md:justify-between">
-                <div class="order-2 md:order-1 md:mb-10">
-                    <h2>
-                        Copyright ©2022 All rights reserved
-                    </h2>
-                </div>
-                <div class="order-1 pb-4 md:order-2 md:mt-10 flex flex-col gap-10 md:flex-row md:items-center">
-                    <p class="text-xl">
-                        Already have an Account? <a class="hover:text-red-600" href="/">Login</a>
-                    </p>
-                    <a href="/">
-                        <button class="btn btn-outline font-bold text-xl btn-secondary md:ml-10">Become a Member</button>
-                    </a>
-                </div>
+            {/* buttom */}
+            <div className="bg-[#D71313] p-6 text-white  text-center text-xl ">
+                <h2>Copyright 2022 VidBox All Rights Reserved</h2>
             </div>
         </footer>
     );
