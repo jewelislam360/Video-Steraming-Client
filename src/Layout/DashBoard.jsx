474acc8f17a78a4973953d9f16f97b4c7b3eb25f
sumon2807 <sharifulbd911@gmail.com>
2023-08-22 23:34:47 +0600

    recode

--- src/Layout/DashBoard.jsx
+++ src/Layout/DashBoard.jsx
@@ -8,8 +8,6 @@ const DashBoard = () => {
             <div className="drawer-content flex flex-col items-center justify-center">
                 {/* Page content here */}
                 <Outlet></Outlet>
-                <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
-
             </div>
             <div className="drawer-side">
                 <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
