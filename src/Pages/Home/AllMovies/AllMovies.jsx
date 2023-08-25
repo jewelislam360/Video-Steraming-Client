60dc55430541655f39f34e736a2c52ca42009256
Abid Hasan Pranto <abidhasanpranto52@gmail.com>
2023-08-25 20:25:49 +0600

    plan redesign

--- src/Pages/Home/AllMovies/AllMovies.jsx
+++ src/Pages/Home/AllMovies/AllMovies.jsx
@@ -13,84 +13,67 @@ import { Pagination } from 'swiper/modules';
 
 
 const AllMovies = () => {
-    const [movies, setMovies] = useState();
-    useEffect(() => {
-        fetch("https://video-streaming-server-sigma.vercel.app/allMovies")
-            .then(res => res.json())
-            .then(data => {
-                setMovies(data);
-            })
-    }, [])
-    return (
-        <div className='my-28 px-20'>
-          <div className='flex justify-between'>
-            <div>
-              <h1 className='font-bold text-4xl'>All Movies</h1>
-            </div>
-            <div>
-              <button className='bg-red-600 px-5 py-3'>More Movies</button>
-            </div>
-          </div>
-            <Swiper
-  
   const [movies, setMovies] = useState();
   useEffect(() => {
-    fetch("Data.json")
+    fetch("https://video-streaming-server-sigma.vercel.app/allMovies")
       .then(res => res.json())
       .then(data => {
         setMovies(data);
       })
   }, [])
   return (
-    <div className='my-64 px-10'>
-      <Swiper
-        slidesPerView={1}
-        spaceBetween={10}
-        pagination={{
-          clickable: true,
-        }}
-        breakpoints={{
-          640: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-          },
-          768: {
-            slidesPerView: 3,
-            spaceBetween: 40,
-          },
-          1024: {
-            slidesPerView: 4,
-            spaceBetween: 50,
-          },
-        }}
-        modules={[Pagination]}
-        className="mySwiper"
-      >
+    
 
-        <div className=''>
+        
+      <div className='my-64 px-10'>
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={10}
+          pagination={{
+            clickable: true,
+          }}
+          breakpoints={{
+            640: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 3,
+              spaceBetween: 40,
+            },
+            1024: {
+              slidesPerView: 4,
+              spaceBetween: 50,
+            },
+          }}
+          modules={[Pagination]}
+          className="mySwiper"
+        >
 
-          {
-            movies?.map(movie => <SwiperSlide><MovieCard
-              key={movie.id}
-              movie={movie}
-            ></MovieCard></SwiperSlide>)
+          <div className=''>
 
-          }
+            {
+              movies?.map(movie => <SwiperSlide><MovieCard
+                key={movie.id}
+                movie={movie}
+              ></MovieCard></SwiperSlide>)
 
-        </div>
+            }
 
-
-      </Swiper>
-
-    </div>
+          </div>
 
 
+        </Swiper>
+
+      </div>
 
 
 
 
 
-  );
+
+
+      );
 };
 
-export default AllMovies;
+      export default AllMovies;
--- src/Pages/Home/Plan/Plan.jsx
+++ src/Pages/Home/Plan/Plan.jsx
@@ -1,185 +1,156 @@
-import React from "react";
-import "./Plan.css";
+import img from "../../../assets/b-img.jpg";
+import './Plan.css'
 import { FaArrowRight } from "react-icons/fa";
 
 const Plan = () => {
   return (
-    <div className="max-w-screen-2xl mx-auto bg-slate-900 my-10 p-10 pt-1 rounded-lg">
-      <h1 className="text-5xl text-white text-center my-7 font-bold">Choose Your Plan</h1>
-      <div className="grid grid-cols-1 lg:grid-cols-4">
-        <div class="text-black bg-white card-body plan">
-          <div className="text-center mx-auto img">
-            <img src="https://i.ibb.co/f0L1m2P/image.png" alt="" srcset="" />
-          </div>
-          <div className="row">
-            <div class="price-details">
-              <span class="price">9</span>
-              <p className="font-medium text-lg">For Daily Plan</p>
-            </div>
-            <div className="">
-              <ul>
-                <span className="flex items-center text-lg">
-                  {" "}
-                  <FaArrowRight />{" "}
-                  <li className="font-medium text-white ">
-                    Access to the entire video library
-                  </li>
-                </span>
-                <span className="flex items-center text-lg mt-2">
-                  {" "}
-                  <FaArrowRight />{" "}
-                  <li className="font-medium text-white ">
-                    HD video quality (up to 1080p)
-                  </li>
-                </span>
-                <span className="flex items-center text-lg mt-2">
-                  {" "}
-                  <FaArrowRight />{" "}
-                  <li className="font-medium text-white ">
-                    Ad-free streaming
-                  </li>
-                </span>
-                <span className="flex items-center text-lg mt-2">
-                  {" "}
-                  <FaArrowRight />{" "}
-                  <li className="font-medium text-white ">
-                    1 Device access
-                  </li>
-                </span>
-              </ul>
-            </div>
-          </div>
-          <button>Get It</button>
-        </div>
+    <div className=" my-9">
+      <h1 className="font-medium text-5xl text-center my-11">Pricing Plan</h1>
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 w-[80%] mx-auto">
 
-        <div class="text-black bg-white card-body plan">
-          <div className="text-center mx-auto img">
-            <img src="https://i.ibb.co/f0L1m2P/image.png" alt="" srcset="" />
-          </div>
-          <div className="row">
-            <div class="price-details">
-              <span class="price2">60</span>
-              <p className="font-medium text-lg">For Weekly Plan</p>
-            </div>
-            <div className="">
-              <ul>
-                <span className="flex items-center text-lg">
-                  {" "}
-                  <FaArrowRight />{" "}
-                  <li className="font-medium text-white ">
-                    Everything in the Daily Pass
-                  </li>
-                </span>
-                <span className="flex items-center text-lg mt-2">
-                  {" "}
-                  <FaArrowRight />{" "}
-                  <li className="font-medium text-white ">
-                    Ultra HD video quality (up to 4K)
-                  </li>
-                </span>
-                <span className="flex items-center text-lg mt-2">
-                  {" "}
-                  <FaArrowRight />{" "}
-                  <li className="font-medium text-white ">
-                    Stream on two devices simultaneously
-                  </li>
-                </span>
-                <span className="flex items-center text-lg mt-2">
-                  {" "}
-                  <FaArrowRight />{" "}
-                  <li className="font-medium text-white ">
-                    Offline downloads on mobile devices
-                  </li>
-                </span>
-              </ul>
-            </div>
-          </div>
-          <button>Get It</button>
-        </div>
 
-        <div class="text-black bg-white card-body plan">
-          <div className="text-center mx-auto img">
-            <img src="https://i.ibb.co/f0L1m2P/image.png" alt="" srcset="" />
+      <div className="gap-5 plan">
+        <div className="card bg-base-100 p-0 shadow-l image-full">
+          <img src={img} alt="movie" />
+          <div className="card-body">
+            <p className="text-4xl text-center">Basic</p>
+            <h2 className="text-6xl font-extrabold text-center">$ 99</h2>
+            <p className="text-center">/Per Month</p>
           </div>
-          <div className="row">
-            <div class="price-details">
-              <span class="price3">250</span>
-              <p className="font-medium text-lg">For Monthly Plan</p>
-            </div>
-            <div className="">
-              <ul>
-                <span className="flex items-center text-lg">
-                  {" "}
-                  <FaArrowRight />{" "}
-                  <li className="font-medium text-white ">
-                    Everything in the Weekly Pass
-                  </li>
-                </span>
-                <span className="flex items-center text-lg mt-2">
-                  {" "}
-                  <FaArrowRight />{" "}
-                  <li className="font-medium text-white ">
-                    Unlimited device streaming
-                  </li>
-                </span>
-                <span className="flex items-center text-lg mt-2">
-                  {" "}
-                  <FaArrowRight />{" "}
-                  <li className="font-medium text-white ">
-                    Exclusive content and early releases
-                  </li>
-                </span>
-                <span className="flex items-center text-lg mt-2">
-                  {" "}
-                  <FaArrowRight />{" "}
-                  <li className="font-medium text-white ">
-                    Offline downloads on mobile devices
-                  </li>
-                </span>
-              </ul>
-            </div>
-          </div>
-          <button>Get It</button>
         </div>
-
-        <div class="text-black bg-white card-body plan">
-          <div className="text-center mx-auto img">
-            <img src="https://i.ibb.co/f0L1m2P/image.png" alt="" srcset="" />
-          </div>
-          <div className="row">
-            <div class="price-details">
-              <span class="price4">2880</span>
-              <p className="font-medium text-lg">For Yearly Plan</p>
-            </div>
-            <div className="">
-              <ul>
-                <span className="flex items-center text-lg">
-                  {" "}
-                  <FaArrowRight />{" "}
-                  <li className="font-medium text-white ">
-                    Everything in the Monthly Pass
-                  </li>
-                </span>
-                <span className="flex items-center text-lg">
-                  {" "}
-                  <FaArrowRight />{" "}
-                  <li className="font-medium text-white ">
-                    Save 17% compared to monthly payments
-                  </li>
-                </span>
-                <span className="flex items-center text-lg ">
-                  {" "}
-                  <FaArrowRight />{" "}
-                  <li className="font-medium text-white ">
-                    One-time payment for a year of premium streaming
-                  </li>
-                </span>
-              </ul>
-            </div>
-          </div>
-          <button>Get It</button>
+        <div className=" mb-2 ">
+        <div className="p-4">
+          <ul>
+            <span className="flex mt-7 items-center text-lg">
+              {" "}
+              <FaArrowRight />{" "}
+              <li className="font-medium text-white ">
+                Everything in the Daily Pass
+              </li>
+            </span>
+            <span className="flex items-center text-lg mt-7">
+              {" "}
+              <FaArrowRight />{" "}
+              <li className="font-medium text-white flex items-center ">
+                Ultra HD video quality (up to 4K)
+              </li>
+            </span>
+            <span className="flex items-center text-lg mt-7">
+              {" "}
+              <FaArrowRight />{" "}
+              <li className="font-medium text-white ">
+                Stream on two devices simultaneously
+              </li>
+            </span>
+            <span className="flex items-center text-lg mt-7">
+              {" "}
+              <FaArrowRight />{" "}
+              <li className="font-medium text-white ">
+                Offline downloads on mobile devices
+              </li>
+            </span>
+          </ul>
+        </div>
+        <div className="card-actions justify-center  pb-3">
+          <button className="btn bg-red-600 border-none">Buy Now</button>
+        </div>
         </div>
       </div>
+      <div className="gap-5 plan">
+        <div className="card bg-base-100 p-0 shadow-l image-full">
+          <img src={img} alt="movie" />
+          <div className="card-body">
+            <p className="text-4xl text-center">Standard</p>
+            <h2 className="text-6xl font-bold text-center">$199</h2>
+            <p className="text-center">/Per Month</p>
+          </div>
+        </div>
+        <div className=" mb-2 ">
+        <div className="p-4">
+          <ul>
+            <span className="flex mt-7 items-center text-lg">
+              {" "}
+              <FaArrowRight />{" "}
+              <li className="font-medium text-white ">
+                Everything in the Daily Pass
+              </li>
+            </span>
+            <span className="flex items-center text-lg mt-7">
+              {" "}
+              <FaArrowRight />{" "}
+              <li className="font-medium text-white ">
+                Ultra HD video quality (up to 4K)
+              </li>
+            </span>
+            <span className="flex items-center text-lg mt-7">
+              {" "}
+              <FaArrowRight />{" "}
+              <li className="font-medium text-white ">
+                Stream on two devices simultaneously
+              </li>
+            </span>
+            <span className="flex items-center text-lg mt-7">
+              {" "}
+              <FaArrowRight />{" "}
+              <li className="font-medium text-white ">
+                Offline downloads on mobile devices
+              </li>
+            </span>
+          </ul>
+        </div>
+        <div className="card-actions justify-center pb-3">
+          <button className="btn bg-red-600 border-none">Buy Now</button>
+        </div>
+        </div>
+      </div>
+      <div className="gap-5 plan">
+        <div className="card bg-base-100 p-0 shadow-l image-full">
+          <img src={img} alt="movie" />
+          <div className="card-body">
+            <p className="text-4xl text-center ">Premium</p>
+            <h2 className="text-6xl font-bold  text-center">$999</h2>
+            <p className="text-center">/Per Year</p>
+          </div>
+        </div>
+        <div className=" mb-2 ">
+        <div className="p-4">
+          <ul>
+            <span className="flex items-center text-lg mt-7">
+              {" "}
+              <FaArrowRight />{" "}
+              <li className="font-medium text-white ">
+                Everything in the Daily Pass
+              </li>
+            </span>
+            <span className="flex items-center text-lg mt-7">
+              {" "}
+              <FaArrowRight />{" "}
+              <li className="font-medium text-white ">
+                Ultra HD video quality (up to 4K)
+              </li>
+            </span>
+            <span className="flex items-center text-lg mt-7">
+              {" "}
+              <FaArrowRight />{" "}
+              <li className="font-medium text-white ">
+                Stream on two devices simultaneously
+              </li>
+            </span>
+            <span className="flex items-center text-lg mt-7">
+              {" "}
+              <FaArrowRight />{" "}
+              <li className="font-medium text-white ">
+                Offline downloads on mobile devices
+              </li>
+            </span>
+          </ul>
+        </div>
+        <div className="card-actions justify-center pb-3">
+          <button className="btn bg-red-600 border-none">Buy Now</button>
+        </div>
+        </div>
+      </div>
+    </div>
     </div>
   );
 };
